605ea66f395ecbf8eae30fc8d161c91dda4042bc
shannoneoff <112975130+shannoneoff@users.noreply.github.com>
2022-09-12 16:48:11 -0700

    Add files from Basic_HTML_and_JavaScript folder
    Added files from Basic_HTML_and_JavaScript folder into HTML-CSS-Projects repository

---
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -0,0 +1,8 @@
+alert("Are you sure you wanna take that path...?");
+
+function My_First_Function() {
+		var String = "Kiss me, I'm Irish!";
+		var result = String.fontcolor("green");
+		document.getElementById("Irish").innerHTML =
+		result;
+	}
