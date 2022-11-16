507a836bd758f3bae3c0adc5b467f9c03d6835c2
shannoneoff <shannon.eoff@gmail.com>
2022-11-15 22:22:42 -0800

    Update Yard Site Project

---
+++ Yard-Site-Project/JS/sculpt.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+
+    $("slideshow > div:gt(0)").hide();
+    
+    setInterval(function () {
+        $('#slideshow > div:first')
+            .fadeOut(1000)
+            .next()
+            .fadeIn(1000)
+            .end()
+            .appendTo('#slideshow');
+    }, 3000);
+});
+
+function openForm() {
+    document.getElementById("contact").style.display = "block";
+}
+
+function closeForm() {
+    document.getElementById("contact").style.display = "none";
+}
